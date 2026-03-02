504e634a3c06ac84501dcbc2cdfa186c6612db9d
Mariusz Gogola <egzamin@poczta.pl>
2026-03-02 13:57:34 +0100

    inf3_4_2026

---
+++ wynik.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl.css">
+    <title>Matura</title>
+</head>
+<body>
+    <header>
+        <h1>System informacji dla maturzystów</h1>
+    </header>
+    <aside>
+        <img src="ma.jpg" alt="Matura"><br>
+        <img src="tu.jpg" alt="Matura"><br>
+        <img src="ra.jpg" alt="Matura">
+    </aside>
+    <section id="sekcja1">
+
+    
+        <!-- SK3 -->
+    </section>
+    <section id="sekcja2">
+        <section class="blok">
+            <h4>Przedmioty</h4>
+        </section>
+        <section class="blok">
+            <h4>Lata</h4>
+        </section>
+        <section class="blok">
+            <h4>Najlepszy wynik</h4>
+        </section>
+         <section class="blok">
+            <h4>Najgorszy wynik</h4>
+        </section>
+    </section>
+    <footer>
+        <p>Numer zdającego: 0010100102323</p>
+    </footer>
+    
+</body>
+</html>
