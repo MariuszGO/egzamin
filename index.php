d769c2a92e5786b1227ceaf2ded89ae2ce9965e5
Mariusz Gogola <egzamin@poczta.pl>
2026-03-06 12:51:45 +0100

    s2

--- index.php
+++ index.php
@@ -15,6 +15,9 @@
         <img src="tu.jpg" alt="Matura"><br>
         <img src="ra.jpg" alt="Matura">
     </aside>
+    <?php 
+        $polaczenie = mysqli_connect("localhost","root","","matura");
+    ?>
     <section id="sekcja1">
 
         <h3>Wybierz ucznia z listy:</h3>
@@ -23,9 +26,27 @@
     <section id="sekcja2">
         <section class="blok">
             <h4>Przedmioty</h4>
+            <?php
+            $zapytanie1 = "SELECT przedmiot FROM arkusz GROUP BY przedmiot";
+            $wynik = mysqli_query($polaczenie,$zapytanie1);
+            while($tablica = mysqli_fetch_array($wynik)){
+                    echo $tablica['przedmiot'] . " ";   
+            }
+            ?>
         </section>
         <section class="blok">
             <h4>Lata</h4>
+              <?php
+            $zapytanie1 = "SELECT min(rok) as najstarszy, max(rok) as najmlodszy FROM `arkusz`";
+            $wynik = mysqli_query($polaczenie,$zapytanie1);
+            while($tablica = mysqli_fetch_array($wynik)){
+                    echo $tablica['najstarszy'] . " - " . $tablica['najmlodszy']; 
+                    
+                    // $zm1 = $tablica['najstarszy'] ;
+                    // $zm2 = $tablica['najmlodszy'];
+                    // echo "$zm1 - $zm2 "; 
+            }
+            ?>
         </section>
         <section class="blok">
             <h4>Najlepszy wynik</h4>
